c1f5068c565c526a7ba09bb45697a8ff7abb38d6
João Souza <marceloamaral201924@gmail.com>
2026-02-09 23:23:17 +0000

    feat: new get endpoint that gets a character by id

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/characters', (req, res) => {
   res.json(list)
 })
 
+app.get('/characters/:id', (req, res) => {
+  const { id } = req.params
+  const character = list[id]
+
+  if (character) {
+    res.json({ id, name: character })
+  } else {
+    res.status(404).json({ error: 'Character not found' })
+  }
+})
+
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
